12d19d7e4ab8aa0ba6abdc1a6c8d530698153773
Kunal Thapa <thapa.ku@northeastern.edu>
2023-04-04 23:27:20 -0400

    comment removal

--- server/server.js
+++ server/server.js
@@ -1,33 +1,12 @@
-
-// const express= require('express');
-// const app = express();
-
-// const cors= require('cors');
-// const mongoose = require('mongoose')
-
 const mainRoute = require('./routes/MainRoute');
-
-// app.use(cors());
-// app.use(express.json());
-
-// mongoose.connect('mongodb://localhost:27017');
-
-
-// app.listen(8000, ()=>{
-//     console.log("Server started at port 8000");
-// })
-
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors= require('cors');
-
 const app = express();
+
 app.use(cors());
 
-
 // Connect to MongoDB
-// mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect("mongodb://127.0.0.1:27017/", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -36,12 +15,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/", { useNewUrlParser: true, useUnifi
     console.log('Error connecting to MongoDB', err);
   });
 
-
 // Parse JSON data
 app.use(express.json());
-// app.use(mainRoute);
 app.use('/user', mainRoute);
-
 
 // Start the server
 app.listen(3000, () => {
